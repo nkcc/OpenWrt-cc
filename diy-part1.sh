4cff2c935f8060894a56f0da23b6ba5643ef91c3
alexcc <nkucc@outlook.com>
2020-11-23 12:56:48 +0800

    Update diy-part1.sh
    去除目录

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # 添加argon主题
-rm -rf lede/package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  lede/package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
